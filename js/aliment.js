0c5659353bae5153394681372bbd2f0025369861
sekon-am <sekon.am@gmail.com>
2014-12-23 14:32:13 +0200

    1.13 again wrong (first animation width)

--- js/aliment.js
+++ js/aliment.js
@@ -1,17 +1,9 @@
 $(function(){
-	$.fn.addWidth = function(delta,minWidth) {
-		var val = $(this).width()+delta;
-		val = (minWidth) ? Math.max(val,minWidth) : val;
-		return $(this).width(val);
-	}
 	jQuery.fn.aliment = function(timeout) {
 		var aliment = $(this),
 			items = aliment.find('*'),
-			inAliment = false,
-			prev = null,
 			self = null,
-			TIME_PER_ACTION = 5,
-			time = 0;
+			locked = false;
 		function getStandardWidth() {
 			return aliment.width()/items.length;
 		}
@@ -21,74 +13,40 @@ $(function(){
 		function getUnactiveWidth() {
 			return Math.round(getStandardWidth()*7/8);
 		}
-		function getActiveWidth() {
-			return aliment.width()-(items.length-1)*getUnactiveWidth();
-		}
-		function getUnactiveDelta() {
-			return Math.round((getStandardWidth()-getUnactiveWidth())*TIME_PER_ACTION/timeout);
-		}
 		function initSize(){
-			prev = null;
 			self = null;
+			locked = false;
 			items.height(getStandardHeight()).width(getStandardWidth());
 		}
 		initSize();
 		$(window)
 			.resize(initSize)
-			.mousemove(function(event){
+			/*.mousemove(function(event){
 				if(
-					aliment.offset().left<=event.pageX &&
-					aliment.offset().top<=event.pageY &&
+					event.pageX>=aliment.offset().left &&
+					event.pageY>=aliment.offset().top &&
 					event.pageX<=aliment.offset().left+aliment.width() &&
 					event.pageY<=aliment.offset().top+aliment.height()
-				){
-					inAliment = true;
-				}else{
-					if(inAliment){
-						items.animate({width:getStandardWidth()},timeout);
-					}
-					inAliment = false;
+				){}else{
+					items.animate({width:getStandardWidth()},timeout,'linear');
+					self = null;
+					console.log(1);
 				}
-			});
+			})*/;
 		
-		function animateAllToOne() {
-			if(time<timeout) {
-				var width=0;
+		items.mousemove(function(){
+			if(this != self && !locked){
+				locked = true;
+				self = this;
+				var width = 0;
 				items.each(function(){
 					if(this != self){
-						$(this).addWidth(-1*getUnactiveDelta(),getUnactiveWidth());
-						width+=$(this).width(); 
+						$(this).animate({width:getUnactiveWidth()},timeout,'linear');
+						width+=getUnactiveWidth(); 
 					}
 				});
-				$(self).width(aliment.width()-width);
-				time+=TIME_PER_ACTION;
-				setTimeout(animateAllToOne,TIME_PER_ACTION);
+				$(this).animate({width:aliment.width()-width},timeout,'linear',function(){locked=false;});
 			}
-		}
-		function animateOneToOne() {
-			$(self).animate({width:getActiveWidth()},timeout);
-			$(prev).animate({width:getUnactiveWidth()},timeout);
-		}
-		items.mousemove(function(){
-			if(inAliment){
-				if(!self){
-					/*time = 0;
-					self = this;
-					animateAllToOne();*/
-					var self = this;
-					items.each(function(){
-						if(this != self){
-							$(this).animate();
-						width+=$(this).width(); 
-					}
-				});
-				}else if(self != this){
-					prev = self;
-					self = this;
-					animateOneToOne();
-				}
-			}
-			return true;
 		});
 		function outputWidth(){
 				var width = 0;
