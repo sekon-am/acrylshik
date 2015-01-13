656eac4640ee1d905a8a62bf721f48137f2107dd
sekon-am <sekon.am@gmail.com>
2015-01-13 12:28:23 +0200

    sub-menu simple opacity

--- js/header.js
+++ js/header.js
@@ -1,10 +1,31 @@
 $(function(){
-	var subMenuDelay = 400;
+	var subMenuDelay = 300,
+		$subMenu = null;
 	$('[data-category-show]').click(function(){
 		var catId = $(this).attr('data-category-show');
-		$('.sub-menu').hide();
-		$('.sub-menu[data-category="'+catId+'"]').slideDown(subMenuDelay);
-		subMenuDelay = 0;
+		if($subMenu){
+			$subMenu.animate(
+				{opacity:0},
+				subMenuDelay,
+				'swing',
+				function(){
+					$subMenu.hide();
+					$subMenu = $('.sub-menu[data-category="'+catId+'"]')
+						.show()
+						.css({opacity:0})
+						.animate(
+							{opacity:1},
+							subMenuDelay,
+							'swing',
+							function (){}
+						);
+				}
+			);
+		}else{
+			$subMenu = $('.sub-menu[data-category="'+catId+'"]')
+				.css({opacity:1})
+				.slideDown(subMenuDelay);
+		}
 		return false;
 	});
 });
